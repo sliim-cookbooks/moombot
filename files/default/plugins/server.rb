44b85cd9368cd522e21312fcda801402417bc0e4
Sliim <sliim@mailoo.org>
2015-11-09 01:30:04 +0100

    Improved server plugin

--- files/default/plugins/server.rb
+++ files/default/plugins/server.rb
@@ -2,44 +2,67 @@
 
 class ServerPlugin
   include Cinch::Plugin
-  timer 60
+  timer 30
   def timer
-    IO.popen("echo get | nc localhost #{moombot[:server][:port]}") do |msgs|
-      msgs.each_line do |msg|
-        Channel(moombot[:cinch][:primary_channel]).send msg
+    client = Client.new
+    client.get.each do |msg|
+      Channel(moombot[:cinch][:primary_channel]).send msg
+    end
+    client.close
+  end
+
+  class Client
+    def initialize()
+      @client = TCPSocket.new '127.0.0.1', moombot[:server][:port]
+    end
+
+    def add(message)
+      @client.puts "add:#{message}"
+    end
+
+    def get
+      @client.puts 'get'
+      result = []
+      while line = @client.gets
+        result << line
+      end
+      return result
+    end
+
+    def close
+      @client.close
+    end
+  end
+
+  class Messages
+    attr_writer :client
+    def initialize
+      @messages = []
+    end
+
+    def query q
+      case q[0..2].to_sym
+      when :add
+        add q[4..-1]
+      when :get
+        get
       end
     end
-  end
-end
 
-class Messages
-  attr_writer :client
-  def initialize
-    @messages = []
-  end
-
-  def query q
-    case q[0..2].to_sym
-    when :add
-      add q[4..-1]
-    when :get
-      get
+    def add(msg)
+      @messages << msg
     end
-  end
 
-  def add(msg)
-    @messages << msg
-  end
-
-  def get
-    @messages.each { |msg| @client.puts msg }
-    @messages = []
+    def get
+      @messages.each { |msg| @client.puts msg }
+      @messages = []
+    end
   end
 end
 
 Daemons.run_proc("#{moombot[:name]}-server", { dir: moombot[:home] }) do
   server = TCPServer.new moombot[:server][:bind_address], moombot[:server][:port]
-  M = Messages.new
+  M = ServerPlugin::Messages.new
   loop do
     Thread.start(server.accept) do |client|
       M.client = client
