338dafce7516bef15f8b9186708e11ad998ce473
Sliim <sliim@mailoo.org>
2020-05-06 18:51:22 +0200

    Update moombot_plugin resource

--- resources/plugin.rb
+++ resources/plugin.rb
@@ -2,8 +2,47 @@
 # Resource:: plugin
 # Copyright:: 2015-2020 Sliim
 
-actions :create, :delete
-default_action :create
 resource_name :moombot_plugin
+provides :moombot_plugin
+default_action :create
 
-attribute :cookbook, kind_of: String, default: 'moombot'
+property :cookbook, String, default: 'moombot'
+
+action :create do
+  unless node['moombot']['plugin_list'].include? new_resource.name
+    node.override['moombot']['plugin_list'] << new_resource.name
+  end
+  cookbook_file "#{node['moombot']['home']}/plugins/#{new_resource.name}.rb" do
+    source "plugins/#{new_resource.name}.rb"
+    cookbook new_resource.cookbook
+    owner node['moombot']['name']
+    group node['moombot']['name']
+    mode '0640'
+    notifies :create, "template[#{node['moombot']['home']}/config.rb]", :immediately
+  end
+
+  begin
+    include_recipe(
+      "#{new_resource.cookbook}::plugin_create_#{new_resource.name}")
+  rescue Chef::Exceptions::RecipeNotFound
+    Chef::Log.info("No recipe for `#{new_resource.name}` plugin action :create")
+  end
+end
+
+action :delete do
+  if node['moombot']['plugin_list'].include? new_resource.name
+    node.override['moombot']['plugin_list'].delete(new_resource.name)
+  end
+
+  cookbook_file "#{node['moombot']['home']}/plugins/#{new_resource.name}.rb" do
+    action :delete
+    notifies :create, "template[#{node['moombot']['home']}/config.rb]", :immediately
+  end
+
+  begin
+    include_recipe(
+      "#{new_resource.cookbook}::plugin_delete_#{new_resource.name}")
+  rescue Chef::Exceptions::RecipeNotFound
+    Chef::Log.info("No recipe for `#{new_resource.name}` plugin action :delete")
+  end
+end
