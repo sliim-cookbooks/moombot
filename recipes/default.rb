956ccc2c676ab864a78c115a65b7a0c748aa9e27
Sliim <sliim@mailoo.org>
2020-05-06 16:29:29 +0200

    Recipes cleanup, keep only default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,97 @@
 # Recipe:: default
 # Copyright:: 2015-2020 Sliim
 
-include_recipe 'moombot::install'
-include_recipe 'moombot::plugins'
-include_recipe 'moombot::configure'
-include_recipe 'moombot::service'
+package 'ruby'
+
+%w(cinch daemons).each do |pkg|
+  gem_package pkg
+end
+
+group node['moombot']['name']
+
+user node['moombot']['name'] do
+  group node['moombot']['name']
+  system true
+  shell '/bin/sh'
+  home node['moombot']['home']
+end
+
+directory node['moombot']['home'] do
+  owner node['moombot']['name']
+  group node['moombot']['name']
+  mode '0750'
+  recursive true
+end
+
+cookbook_file "#{node['moombot']['home']}/daemon" do
+  source 'daemon.rb'
+  owner node['moombot']['name']
+  group node['moombot']['name']
+  mode '0750'
+end
+
+directory "#{node['moombot']['home']}/plugins" do
+  owner node['moombot']['name']
+  group node['moombot']['name']
+  mode '0750'
+end
+
+if node['moombot']['plugins'].is_a? ::Chef::Node::ImmutableArray
+  node.override['moombot']['plugin_list'] = node['moombot']['plugins']
+  node['moombot']['plugins'].each do |name|
+    moombot_plugin name
+  end
+elsif node['moombot']['plugins'].is_a? ::Chef::Node::ImmutableMash
+  node.override['moombot']['plugin_list'] =
+    node['moombot']['plugins'].map { |_k, v| v }.flatten
+  node['moombot']['plugins'].each do |cookbook_name, plugins|
+    plugins.each do |name|
+      moombot_plugin name do
+        cookbook cookbook_name
+      end
+    end
+  end
+else
+  Chef::Log.warn('Cannot determine plugins list, '\
+                 'attribute must be an Array or a Hash')
+end
+
+template "#{node['moombot']['home']}/config.rb" do
+  source 'config.rb.erb'
+  owner node['moombot']['name']
+  group node['moombot']['name']
+  mode '0640'
+end
+
+if node['init_package'] == 'systemd'
+  template "/etc/systemd/system/#{node['moombot']['name']}.service" do
+    source 'service-systemd.erb'
+    owner node['moombot']['name']
+    group node['moombot']['name']
+    mode '0644'
+    notifies :run, 'execute[systemctl-daemon-reload]', :immediately
+    notifies :restart, "service[#{node['moombot']['name']}]", :delayed
+  end
+else
+  template "/etc/init.d/#{node['moombot']['name']}" do
+    source 'service-init.erb'
+    owner node['moombot']['name']
+    group node['moombot']['name']
+    mode '0755'
+    notifies :restart, "service[#{node['moombot']['name']}]", :delayed
+  end
+end
+
+service node['moombot']['name'] do
+  action :enable
+  supports status: true, start: true, stop: true, restart: true
+  subscribes :restart,
+             "cookbook_file[#{node['moombot']['home']}/daemon]", :delayed
+  subscribes :restart,
+             "template[#{node['moombot']['home']}/config.rb]", :delayed
+end
+
+execute 'systemctl-daemon-reload' do
+  action :nothing
+  command 'systemctl daemon-reload'
+end
--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -3,9 +3,161 @@ require_relative 'spec_helper'
 describe 'moombot' do
   subject { ChefSpec::SoloRunner.new.converge(described_recipe) }
 
-  %w(install plugins configure service).each do |recipe|
-    it "includes recipe[moombot::#{recipe}]" do
-      expect(subject).to include_recipe("moombot::#{recipe}")
+  it 'installs package[ruby]' do
+    expect(subject).to install_package('ruby')
+  end
+
+  %w(cinch daemons).each do |pkg|
+    it "installs gem_package[#{pkg}]" do
+      expect(subject).to install_gem_package(pkg)
+    end
+  end
+
+  it 'creates group[moombot]' do
+    expect(subject).to create_group('moombot')
+  end
+
+  it 'creates user[moombot]' do
+    expect(subject).to create_user('moombot')
+      .with(system: true,
+            group: 'moombot',
+            shell: '/bin/sh',
+            home: '/opt/moombot')
+  end
+
+  it 'creates directory[/opt/moombot]' do
+    expect(subject).to create_directory('/opt/moombot')
+      .with(owner: 'moombot',
+            group: 'moombot',
+            mode: '0750',
+            recursive: true)
+  end
+
+  it 'creates cookbook_file[/opt/moombot/daemon]' do
+    expect(subject).to create_cookbook_file('/opt/moombot/daemon')
+      .with(source: 'daemon.rb',
+            owner: 'moombot',
+            group: 'moombot',
+            mode: '0750')
+  end
+
+  it 'creates directory[/opt/moombot/plugins]' do
+    expect(subject).to create_directory('/opt/moombot/plugins')
+      .with(owner: 'moombot',
+            group: 'moombot',
+            mode: '0750')
+  end
+
+  context 'plugin lwrp with default attributes' do
+    let(:subject) do
+      ChefSpec::SoloRunner.new(
+        step_into: ['moombot_plugin']).converge described_recipe
+    end
+
+    %w(ping server).each do |plugin|
+      it "creates moombot_plugin[#{plugin}]" do
+        expect(subject).to create_moombot_plugin(plugin)
+      end
+    end
+
+    it 'creates cookbook_file[/opt/moombot/plugins/ping.rb]' do
+      expect(subject).to create_cookbook_file('/opt/moombot/plugins/ping.rb')
+        .with(source: 'plugins/ping.rb',
+              cookbook: 'moombot',
+              owner: 'moombot',
+              group: 'moombot',
+              mode: '0640')
+    end
+
+    it 'creates cookbook_file[/opt/moombot/plugins/server.rb]' do
+      expect(subject).to create_cookbook_file('/opt/moombot/plugins/server.rb')
+        .with(source: 'plugins/server.rb',
+              cookbook: 'moombot',
+              owner: 'moombot',
+              group: 'moombot',
+              mode: '0640')
+    end
+  end
+
+  context 'plugins attribute is kind of Array' do
+    let(:subject) do
+      ChefSpec::SoloRunner.new do |node|
+        node.override['moombot']['plugins'] = %w(plugin1 plugin2)
+      end.converge described_recipe
+    end
+
+    [1, 2].each do |i|
+      it "creates moombot_plugin[plugin#{i}]" do
+        expect(subject).to create_moombot_plugin("plugin#{i}")
+      end
+    end
+  end
+
+  context 'plugins attribute is kind of Hash' do
+    let(:subject) do
+      ChefSpec::SoloRunner.new do |node|
+        node.override['moombot']['plugins'] = { moombot: %w(plugin1 plugin2),
+                                                devbot: %w(plugin3 plugin4) }
+      end.converge described_recipe
+    end
+
+    [1, 2].each do |i|
+      it "creates moombot_plugin[plugin#{i}]" do
+        expect(subject).to create_moombot_plugin("plugin#{i}")
+          .with(cookbook: 'moombot')
+      end
+    end
+
+    [3, 4].each do |i|
+      it "creates moombot_plugin[plugin#{i}]" do
+        expect(subject).to create_moombot_plugin("plugin#{i}")
+          .with(cookbook: 'devbot')
+      end
+    end
+  end
+
+  it 'creates template[/opt/moombot/config.rb]' do
+    expect(subject).to create_template('/opt/moombot/config.rb')
+      .with(source: 'config.rb.erb',
+            owner: 'moombot',
+            group: 'moombot',
+            mode: '0640')
+  end
+
+  it 'enables service[moombot]' do
+    expect(subject).to enable_service('moombot')
+  end
+
+  context 'with systemd' do
+    let(:subject) do
+      ChefSpec::SoloRunner.new do |node|
+        node.automatic['init_package'] = 'systemd'
+      end.converge(described_recipe)
+    end
+
+    it 'creates template[/etc/systemd/system/moombot.service]' do
+      service_file = '/etc/systemd/system/moombot.service'
+      expect(subject).to create_template(service_file)
+        .with(source: 'service-systemd.erb',
+              owner: 'moombot',
+              group: 'moombot',
+              mode: '0644')
+    end
+  end
+
+  context 'without systemd' do
+    let(:subject) do
+      ChefSpec::SoloRunner.new do |node|
+        node.automatic['init_package'] = 'init'
+      end.converge(described_recipe)
+    end
+
+    it 'creates template[/etc/init.d/moombot]' do
+      expect(subject).to create_template('/etc/init.d/moombot')
+        .with(source: 'service-init.erb',
+              owner: 'moombot',
+              group: 'moombot',
+              mode: '0755')
     end
   end
 end
